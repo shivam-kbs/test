6ece582930612dcb0ea5148a9a2a7d669429aeac
shivam-kbs <shivam-kbs>
2018-03-09 10:39:42 +0530

    json add

---
+++ jqueryTest.php
@@ -0,0 +1,30 @@
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	  <title>bsdsb</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+
+	<div class="container">
+         <table class="table table-striped table-hover ">
+     	    <thead>
+     	      <tr>
+     	        <th>resr</th>
+     	      </tr>
+     	    </thead>
+     	    <tbody>
+     	      <tr>
+     	        <td>weerwe</td>
+     	      </tr>
+     	    </tbody>
+         </table>
+      eerreeee	
+	</div>
+
+	</body>
+	</html>
